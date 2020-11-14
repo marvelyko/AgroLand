1f5d07c3e47926c798354c6d7e3bbe448d3af8aa
tengopataraia123 <pataraiamistera@gmail.com>
2020-11-14 18:53:27 +0400

    update
    update

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,url_for,sessions,redirect
 from flask_mail import Mail, Message
 from myproject.forms import PlantsForms
-#from myproject.methods import calculate
+#from myproject.test import choise_point,yes_no,precipitation,minfrost,mintemp
+from collections import Counter
+
 import pandas as pd
 app = Flask(__name__)
 mail= Mail(app)
@@ -15,25 +17,139 @@ app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 
-@app.route('/' )
-def main():
+@app.route('/',methods=['GET', 'POST'] )
+def index():
+    names = ['plant','plant','plant','plant']
+    scores = ['0','0','0','0']
+    res=True
     form=PlantsForms()
+    if form.validate_on_submit():
+        res=False
+        df = pd.read_csv('data.csv', encoding='utf8')
 
-    df = pd.read_csv('data.csv', encoding='utf8')
+        samp = df.iloc[2][1:]
+        samp['Adapted to Coarse Textured Soils'] = form.Coarse.data
+        samp['Adapted to Medium Textured Soils'] = form.Medium.data
+        samp['Adapted to Fine Textured Soils'] = form.Fine.data
+        samp['CaCO<SUB>3</SUB> Tolerance'] = form.CaCO.data
+        samp['Cold Stratification Required'] = form.Cold.data
+        samp['Frost Free Days, Minimum'] = form.Frost_free.data
+        samp['Moisture Use'] = form.Moisture.data
+        samp['pH (Minimum)'] = form.pH_Min.data
+        samp['pH (Maximum)'] = form.pH_Max.data
+        samp['Precipitation (Minimum)'] = form.Precipitation_min.data
+        samp['Precipitation (Maximum)'] = form.Precipitation_max.data
+        samp['Temperature, Minimum (°F)'] = form.temperature_min.data
 
-    #
+
+        def choise_point(col, plant, samp):
+            choise = {'Any': 'Any', 'None': 0, 'Low': 1,
+                      'Medium': 2, 'High': 3}
+            try:
+
+                point_samp = choise[samp[col]]
+                point_df = choise[plant[col]]
+                if plant[col] == samp[col] or samp[col] == 'Any':
+                    return 1
+
+                return 1 / (point_df - point_samp + 2) ** 2
+            except:
+                return 0.1
+
+        def yes_no(col, plant, samp):
+            if plant[col] == samp[col] or samp[col] == 'Any':
+                return 1
+            return 0
+
+        def precipitation(plant, samp):
+            col_min = samp['Precipitation (Minimum)']
+            print(samp)
+            col_max = samp['Precipitation (Maximum)']
+            plant_min = plant['Precipitation (Minimum)']
+            plant_max = plant['Precipitation (Maximum)']
+
+            if col_min >= plant_min and col_max <= plant_max:
+                return 1
+            k = 0
+            if col_min < plant_min:
+                k += plant_min - col_min
+            if col_max > plant_max:
+                k += col_max - plant_max
+            return 2 / (k + 3)
+
+        def mintemp(plant, samp):
+            col_t = samp['Temperature, Minimum (°F)'] * (9 / 5) + 32
+
+            plant_t = plant['Temperature, Minimum (°F)']
+            if col_t >= plant_t:
+                return 1
+            return 5 / (-col_t + plant_t + 10)
+
+        def minfrost(col, plant, samp):
+            col_f = samp[col]
+            plant_f = plant[col]
+            if col_f >= plant_f:
+                return 1
+            return 15 / (-col_f + plant_f + 40)
+
+        def countpoint(plant, samp):
+            s = 0
+            plant = plant
+            #########
+            s += yes_no('Adapted to Coarse Textured Soils', plant, samp)
+
+            #########
+            s += yes_no('Adapted to Medium Textured Soils', plant, samp)
+
+            ###########
+            s += yes_no('Adapted to Fine Textured Soils', plant, samp)
+
+            ###################################
+            # s += choise_point('Anaerobic Tolerance', plant, samp)
+
+            #################################
+            s += choise_point('CaCO<SUB>3</SUB> Tolerance', plant, samp)
+
+            ##################################
+            s += yes_no('Cold Stratification Required', plant, samp)
+
+            ##################################
+            s += choise_point('Moisture Use', plant, samp)
+
+            ##################################
+            s += precipitation(plant, samp)
+
+            ##############
+            s += mintemp(plant, samp)
+
+            ############
+            s += minfrost('Frost Free Days, Minimum', plant, samp)
+
+            #s += precipitation('pH (Minimum)', 'pH (Maximum)', plant, samp)
+
+            return s
+
+        def calculate(df, samp):
+            result = {}
+            for plant in df.iloc:
+                #result[plant[0]] = countpoint(plant, samp) / 11 * 100
+
+                try:
+                    result[plant[0]] = countpoint(plant, samp) / 11 * 100
+                except:
+                    result[plant[0]] = 0
+
+            return dict(Counter(result).most_common(10))
+
+        result = calculate(df, samp)
+        names = list(result.keys())
+        scores = list(result.values())
+        scores=[round(x) for x in scores ]
 
 
-
-
-
-
-
-
-
-    return render_template('index.html',title="Company Name",form=form)
-
-
+        return render_template('index.html',title="Company Name",form=form,names=names,scores=scores,res=res)
+    k="49"
+    return render_template('index.html',title="Company Name",form=form,k=k,res=res,names=names,scores=scores)
 
 
 @app.route('/contact', methods=['GET', 'POST'])
@@ -51,5 +167,9 @@ def contact():
         msg.title = 'title'
         mail.send(msg)
     return render_template('contact.html',title='contact')
+
+
+
+
 
 
--- myproject/methods.py
+++ myproject/methods.py
@@ -1,8 +1,11 @@
 
+
+
+
 from collections import Counter
 from myproject.forms import PlantsForms
 form=PlantsForms()
-def choise_point(col):
+def choise_point(col,plant,samp):
     choise = {'Any': 'Any', 'None': 0, 'Low': 1,
               'Medium': 2, 'High': 3}
     try:
@@ -17,13 +20,13 @@ def choise_point(col):
         return 0.1
 
 
-def yes_no(col):
+def yes_no(col,plant,samp):
     if plant[col] == samp[col] or samp[col] == 'Any':
         return 1
     return 0
 
 
-def precipitation(colmin, colmax):
+def precipitation(colmin, colmax,plant,samp):
     col_min = samp[colmin]
     col_max = samp[colmax]
     plant_min = plant[colmin]
@@ -39,7 +42,7 @@ def precipitation(colmin, colmax):
     return 2 / (k + 3)
 
 
-def mintemp(col):
+def mintemp(col,plant,samp):
     col_t = samp[col] * (9 / 5) + 32
 
     plant_t = plant[col]
@@ -48,7 +51,7 @@ def mintemp(col):
     return 5 / (-col_t + plant_t + 10)
 
 
-def minfrost(col):
+def minfrost(col,plant,samp):
     col_f = samp[col]
     plant_f = plant[col]
     if col_f >= plant_f:
@@ -56,32 +59,33 @@ def minfrost(col):
     return 15 / (-col_f + plant_f + 40)
 
 
-def countpoint(plant):
+def countpoint(plant,samp):
     s = 0
+    plant=plant
+    samp=samp
+    #########
+    s += yes_no('Adapted to Coarse Textured Soils',plant,samp)
 
     #########
-    s += yes_no('Adapted to Coarse Textured Soils')
-
-    #########
-    s += yes_no('Adapted to Medium Textured Soils')
+    s += yes_no('Adapted to Medium Textured Soils',plant,samp)
 
     ###########
-    s += yes_no('Adapted to Fine Textured Soils')
+    s += yes_no('Adapted to Fine Textured Soils',plant,samp)
 
     ###################################
-    s += choise_point('Anaerobic Tolerance')
+    s += choise_point('Anaerobic Tolerance',plant,samp)
 
     #################################
-    s += choise_point('CaCO<SUB>3</SUB> Tolerance')
+    s += choise_point('CaCO<SUB>3</SUB> Tolerance',plant,samp)
 
     ##################################
-    s += yes_no('Cold Stratification Required')
+    s += yes_no('Cold Stratification Required',plant,samp)
 
     ##################################
-    s += choise_point('Moisture Use')
+    s += choise_point('Moisture Use',plant,samp)
 
     ##################################
-    s += precipitation('Precipitation (Minimum)', 'Precipitation (Maximum)')
+    s += precipitation('Precipitation (Minimum)', 'Precipitation (Maximum)',plant,samp)
 
     ##############
     s += mintemp('Temperature, Minimum (°F)')
@@ -93,40 +97,43 @@ def countpoint(plant):
 
     return s
 
-def calculate(df):
+def calculate(df,plant):
+    form=PlantsForms()
+    plant=plant
+    samp = creatSamp(form,df)
 
     result = {}
     for plant in df.iloc:
         try:
 
-            result[plant[0]] = countpoint(plant) / 11 * 100
+            result[plant[0]] = countpoint(plant,samp) / 11 * 100
         except:
             print(f"{plant[0]}")
 
     return dict(Counter(result).most_common(10))
 
-# def creatSamp(form,df):
-#     samp = df.iloc[2][1:]
-#     samp['Adapted to Coarse Textured Soils'] = form.Coarse.data
-#     samp['Adapted to Medium Textured Soils'] = form.Medium.data
-#     samp['Adapted to Fine Textured Soils'] = form.Fine.data
-#     #samp['Anaerobic Tolerance'] = form.Anaerobic.data
-#     samp['CaCO<SUB>3</SUB> Tolerance'] = form.CaCO.data
-#     samp['Cold Stratification Required'] = form.Cold.data
-#     #samp['Drought Tolerance'] = form.Drought.data
-#     #samp['Fertility Requirement'] = form.Fertility.data
-#     #samp['Fire Tolerance'] = form.Fire.data
-#     samp['Frost Free Days, Minimum'] = form.Frost_free.data
-#     samp['Moisture Use'] = form.Moisture.data
-#     #samp['Hedge Tolerance'] = form.Hedge.data
-#     samp['pH (Minimum)'] = form.pH_Min.data
-#     samp['pH (Maximum)'] = form.pH_Max.data
-#     #samp['Planting Density per Acre, Minimum'] = form.plant_density_min.data
-#     #samp['Planting Density per Acre, Maximum '] = form.plant_density_max.data
-#     samp['Precipitation (Minimum)'] = form.Precipitation_min.data
-#     samp['Precipitation (Maximum)'] = form.Precipitation_max.data
-#     #samp['Root Depth, Minimum (inches)'] = form.root_depth_min.data
-#     #samp['Salinity Tolerance'] = form.Salinity.data
-#     #samp['Shade Tolerance'] = form.Shade.data
-#     samp['Temperature, Minimum (°F)'] = form.temperature_min.data
-#     return samp
+def creatSamp(form,df):
+    samp = df.iloc[2][1:]
+    samp['Adapted to Coarse Textured Soils'] = form.Coarse.data
+    samp['Adapted to Medium Textured Soils'] = form.Medium.data
+    samp['Adapted to Fine Textured Soils'] = form.Fine.data
+    #samp['Anaerobic Tolerance'] = form.Anaerobic.data
+    samp['CaCO<SUB>3</SUB> Tolerance'] = form.CaCO.data
+    samp['Cold Stratification Required'] = form.Cold.data
+    #samp['Drought Tolerance'] = form.Drought.data
+    #samp['Fertility Requirement'] = form.Fertility.data
+    #samp['Fire Tolerance'] = form.Fire.data
+    samp['Frost Free Days, Minimum'] = form.Frost_free.data
+    samp['Moisture Use'] = form.Moisture.data
+    #samp['Hedge Tolerance'] = form.Hedge.data
+    samp['pH (Minimum)'] = form.pH_Min.data
+    samp['pH (Maximum)'] = form.pH_Max.data
+    #samp['Planting Density per Acre, Minimum'] = form.plant_density_min.data
+    #samp['Planting Density per Acre, Maximum '] = form.plant_density_max.data
+    samp['Precipitation (Minimum)'] = form.Precipitation_min.data
+    samp['Precipitation (Maximum)'] = form.Precipitation_max.data
+    #samp['Root Depth, Minimum (inches)'] = form.root_depth_min.data
+    #samp['Salinity Tolerance'] = form.Salinity.data
+    #samp['Shade Tolerance'] = form.Shade.data
+    samp['Temperature, Minimum (°F)'] = form.temperature_min.data
+    return samp
